31fad39645c6de5f73f7703b4a93a682aaf337d3
sanjeev <sanjeevkumarsen74@gmail.com>
2025-07-02 23:13:51 +0530

    American code

---
+++ src/test/java/resources/TestSprit.java
@@ -0,0 +1,21 @@
+package resources;
+
+public class TestSprit {
+
+	public static void main(String[] args) {
+
+		System.out.println("American client code--");
+		
+		System.out.println("American client code--");
+		
+		
+		System.out.println("American client code--");
+		
+		System.out.println("Client code from Russia----");
+		
+		System.out.println("Client code from Russia----");
+		System.out.println("Client code from Russia----");
+		System.out.println("Client code from Russia----");
+	}
+
+}
